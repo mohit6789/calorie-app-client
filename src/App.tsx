547f595910ef5b1a417dea19bdaa57e0bd52f5c2
Adam Zdrzalka <adamzdrzalka@outlook.com>
2022-02-26 14:34:47 +0530

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="">
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
