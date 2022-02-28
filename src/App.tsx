1c585449b67af94387c7d233ea7185458642219d
Adam Zdrzalka <adamzdrzalka@outlook.com>
2022-02-28 10:51:55 +0530

    common services and added packages

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Unauthorised from "./pages/Unauthorised";
 import PrivateRoute from "./components/PrivateRoute";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/unauthorised" element={<Unauthorised />} />
-      </Routes>
+      <Box m={4}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/unauthorised" element={<Unauthorised />} />
+        </Routes>
+      </Box>
     </div>
   );
 }
--- src/services/backend.service.ts
+++ src/services/backend.service.ts
@@ -1,3 +1,45 @@
-export function get() {
-  
+import axios from "axios";
+import storage from "./storage.service";
+import { USER_TOKEN_KEY } from "../contants";
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "";
+
+const http = axios.create();
+
+function getBackendUrl(url: string) {
+  return `${BASE_URL}/${url}`;
+}
+
+function getBackendHeaders(headers: any) {
+  const accessToken = storage.getItem(USER_TOKEN_KEY);
+  const authHeader = { "x-access-token": accessToken };
+  return { ...authHeader, ...headers };
+}
+
+function get(url: string, headers: any = {}, params: any = {}) {
+  return http.get(getBackendUrl(url), {
+    headers: getBackendHeaders(headers),
+    ...params,
+  });
+}
+
+function post(url: string, data: any, headers: any = {}, params: any = {}) {
+  return http.post(getBackendUrl(url), data, {
+    headers: getBackendHeaders(headers),
+    ...params,
+  });
+}
+
+function patch(url: string, data: any, headers: any = {}, params: any = {}) {
+  return http.patch(getBackendUrl(url), data, {
+    headers: getBackendHeaders(headers),
+    ...params,
+  });
+}
+
+function remove(url: string, data: any, headers: any = {}, params: any = {}) {
+  return http.delete(getBackendUrl(url), {
+    headers: getBackendHeaders(headers),
+    ...params,
+  });
 }
